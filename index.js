4f375aa50f1da3c0b0d80974468567015fff127c
tor <torfrancis447@gmail.com>
2022-11-04 07:57:56 -0400

    commit tree 43b0e3f04b9bfc77ec9f9fc9d47f741b1b8d97ac

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const p= document.querySelector('#text')
+    p.textContent = 'This is really cool!'
+})
+
+console.log("the dom is loaded 2")
